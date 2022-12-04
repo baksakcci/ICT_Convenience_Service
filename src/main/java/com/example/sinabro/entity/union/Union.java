846483f165ee427467146e3f874e80a11dadcded
pshsh910 <pshsh910@gmail.com>
2022-12-04 23:02:38 +0900

    feat: 학생회실 개폐 여부 Union 추가

--- src/main/java/com/example/sinabro/entity/union/Union.java
+++ src/main/java/com/example/sinabro/entity/union/Union.java
@@ -26,3 +26,16 @@ public class Union {
     }
 
 }
+
+@Converter
+class BooleanToYNConverter implements AttributeConverter<Boolean, String>{
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute){
+        return (attribute != null && attribute) ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String dbData){
+        return "Y".equals(dbData);
+    }
+}
