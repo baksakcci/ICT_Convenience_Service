c7fc0d8657cd44c656d53e139eb44a85dcacd262
pshsh910 <pshsh910@gmail.com>
2022-12-04 16:28:33 +0900

    refactor: createItem()에서 .orElseThrow 대신 .get()으로 변경

--- src/main/java/com/example/sinabro/service/AdminService.java
+++ src/main/java/com/example/sinabro/service/AdminService.java
@@ -108,7 +108,7 @@ public class AdminService {
 
     @Transactional
     public void createItem(ItemRequestDto ite) {
-        Item item1 = itemRepository.findByItemDetailName(ite.getItemDetailName()).orElseThrow(ItemNotFoundException::new);
+        Item item1 = itemRepository.findByItemDetailName(ite.getItemDetailName()).get();
         if(item1.getItemDetailName().equals(ite.getItemDetailName())) {
             throw new ItemDontCreateException();
         }
